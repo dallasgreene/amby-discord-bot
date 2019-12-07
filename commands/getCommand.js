d6522761254b3ade26d09f0f07a90f6d0b6b74a3
dallasgreene <rkdaproxy@gmail.com>
2019-12-06 21:56:17 -0500

    trying to get role info

--- commands/getCommand.js
+++ commands/getCommand.js
@@ -1,12 +1,17 @@
 const message = require("../functions/message");
 
 module.exports = {
-    go: (guild, rest) => {
+    go: (msg, rest) => {
         if (rest[0] === "roleSnow") {
-            const keys = guild.roles.keyArray();
+            const roles = msg.channel.guild.roles;
+            const keys = roles.keyArray();
             keys.forEach(key => {
-                console.log(`${key} : ${guild.roles.get(key)}`);
+                message.send(msg.channel, `${roles.get(key)}`);
+                console.log(`${key} : ${roles.get(key)}`);
             });
+        }
+        else if (rest[0] === "myRoles") {
+
         }
     }
 };
