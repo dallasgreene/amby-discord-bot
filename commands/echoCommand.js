565839e9e7216d8a5e1cfc4ddfdea688bae134dd
dallasgreene <rkdaproxy@gmail.com>
2019-12-13 00:59:49 -0700

    added usage

--- commands/echoCommand.js
+++ commands/echoCommand.js
@@ -4,5 +4,6 @@ module.exports = {
     go: (msg, rest) => {
         if (rest.includes("@everyone")) message.send(msg.channel, "stop it.");
         message.send(msg.channel, rest.join(" "));
-    }
+    },
+    usage: prefix => `${prefix}echo <message>`
 };
