9de1de54c45b7080c3709181f3c2dfca32334526
dallasgreene <rkdaproxy@gmail.com>
2019-12-13 00:59:31 -0700

    began implementing

--- commands/helpCommand.js
+++ commands/helpCommand.js
@@ -1,21 +1,21 @@
-const hey = require("./heyCommand");
-const role = require("./roleCommand");
-const emoji = require("./emojiCommand");
-const play = require("./playCommand");
-const add = require("./addCommand");
-const grind = require("./grindCommand");
-const echo = require("./echoCommand");
-const get = require("./getCommand");
+const { RichEmbed } = require('discord.js');
+const { prefix } = require('../config');
+const message = require('../functions/message');
 
+module.exports = commandList => {
+    const go = (msg, rest) => {
+        let helpMsg = new RichEmbed()
+            .setColor('#0099ff')
+            .setTitle("Available Commands:");
 
+        for (let command in commandList) {
+            helpMsg.addField(`${prefix}${command}`, `snippet`);
+        }
 
-module.exports = {
-    hey: (msg, rest) => hey.snippet(msg.channel),
-    role: (msg, rest) => role.snippet(msg, rest),
-    emoji: (msg, rest) => emoji.snippet(msg, rest),
-    play: (msg, rest) => play.snippet(msg.channel, rest),
-    add: (msg, rest) => add.snippet(msg.channel, rest),
-    grind: (msg, rest) => grind.snippet(msg.channel, rest),
-    echo: (msg, rest) => echo.snippet(msg.channel, rest),
-    get: (msg, rest) => get.snippet(msg, rest)
+        message.send(msg.channel, helpMsg);
+    };
+
+    return {
+        go
+    };
 };
