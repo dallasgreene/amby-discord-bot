c66f2d1901c9ebe63c98886736b993594563f483
dallasgreene <rkdaproxy@gmail.com>
2019-12-29 23:32:18 -0700

    refactored help command to include itself

--- src/commands/helpCommand.js
+++ src/commands/helpCommand.js
@@ -4,22 +4,26 @@ const message = require('../functions/message');
 const Command = require('../models/Command');
 
 module.exports = commandList => {
+    const usage = "help <command>";
+    const snippet = "get details on how to use a command";
+    const helpText = `Specify a command and Amby will tell you how to use it. `
+        + `If you just use ${prefix}help, Amby will give you a list of available commands.`;
+
     const go = (msg, rest) => {
         let helpMsg = new RichEmbed()
             .setColor('#0099ff')
             .setTitle("Available Commands:");
 
-        Object.keys(commandList).forEach(cmd => {
-            helpMsg.addField(`${commandList[cmd].usage(prefix)}`, `${commandList[cmd].snippet}`);
-        });
+        for (let cmd in commandList) {
+            if (commandList.hasOwnProperty(cmd)) {
+                helpMsg.addField(commandList[cmd].usage(prefix), commandList[cmd].snippet);
+            }
+        }
+
+        helpMsg.addField(usage, snippet);
 
         message.send(msg.channel, helpMsg);
     };
 
-    return new Command(
-        go,
-        "help <command>",
-        "get details on how to use a command",
-        `Specify a command and Amby will tell you how to use it. `
-        + `If you just use ${prefix}help, Amby will give you a list of available commands.`);
+    return new Command(go, usage, snippet, helpText);
 };
