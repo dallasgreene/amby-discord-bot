39983011a6a579b486e17077287ed87c75b0fe7b
dallasgreene <rkdaproxy@gmail.com>
2020-09-27 23:05:56 -0400

    docker bugfixes, make it easier to spin up db

--- db/startup/00_init_db.sh
+++ db/startup/00_init_db.sh
@@ -2,6 +2,8 @@
     const MONGO_INITDB_ROOT_USERNAME = $(_js_escape "$MONGO_INITDB_ROOT_USERNAME");
     const MONGO_INITDB_ROOT_PASSWORD = $(_js_escape "$MONGO_INITDB_ROOT_PASSWORD");
 
+    db = db.getSiblingDB('admin');
+
     db.auth(MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD);
 
     db = db.getSiblingDB('amby-db');
@@ -9,13 +11,7 @@
     db.createUser({
         user: MONGO_INITDB_ROOT_USERNAME,
         pwd: MONGO_INITDB_ROOT_PASSWORD,
-        roles: [ 'readWrite' ],
-        authenticationRestrictions: [
-            {
-                clientSource: [ '127.0.0.1' ],
-                serverAddress: [ '127.0.0.1' ]
-            }
-        ]
+        roles: [{ role: 'readWrite', db: 'amby-db' }]
     });
 
     db.createDate.insertOne({ time: Date.now() });
